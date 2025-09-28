455790238d5641e99fa1fe8e75c10732b2e31075
nguyendangviet12022004 <viet.ngdang.dev@gmail.com>
2025-09-28 19:08:40 +0700

    chore: initial commit with README and project structure

---
+++ src/main/java/com/swd/e_bake_ingredients/EBakeIngredientsApplication.java
@@ -0,0 +1,13 @@
+package com.swd.e_bake_ingredients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBakeIngredientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBakeIngredientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swd/e_bake_ingredients/EBakeIngredientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swd.e_bake_ingredients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBakeIngredientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
