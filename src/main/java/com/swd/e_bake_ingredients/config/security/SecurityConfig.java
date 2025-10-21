04715ce7f53245d75ce7c54c9ddc1c6274b1eccf
nguyendangviet12022004 <viet.ngdang.dev@gmail.com>
2025-10-21 11:15:16 +0700

    fix(nginx): fix post method not allowed: - disable csrf - config forward spring

--- src/main/java/com/swd/e_bake_ingredients/config/security/SecurityConfig.java
+++ src/main/java/com/swd/e_bake_ingredients/config/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -34,6 +35,8 @@ public class SecurityConfig {
                         .invalidateHttpSession(true)
                         .deleteCookies("JSESSIONID")
                         .logoutSuccessUrl("/auth/login?logout=true"));
+
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
